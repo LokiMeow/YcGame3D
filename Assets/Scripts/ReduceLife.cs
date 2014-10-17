6ae0d59787c4f06adfad76788f622d5757255fd1
LokiMeow <inuyasya030@163.com>
2014-10-18 00:16:41 +0800

    大致弄好了减血效果,差调用,感觉还有点问题
    上升效果还有点问题,调了几个消失时间感觉都差不多

--- Assets/Scripts/ReduceLife.cs
+++ Assets/Scripts/ReduceLife.cs
@@ -10,23 +10,40 @@ public class ReduceLife : MonoBehaviour {
 	/// <summary>
 	/// 定时消失
 	/// </summary>
-	public float timer=3;
+	public float timer=1.0f;
 
 	Transform m_transform;
+	
+	Vector3 charactorPosition;
+	float up=0;
 
 	// Use this for initialization
 	void Start () {
 		m_transform=this.transform;
+		
+		//m_transform.position=Camera.main.WorldToViewportPoint(GameObject.FindGameObjectWithTag("Player").transform.position);
 	}
 	
 	// Update is called once per frame
 	void Update () {
+	
+		if(timer<=0)
+			Destroy(this.gameObject);
+			
 		if(text!="")
 		{
 			timer-=Time.deltaTime;
 			m_transform.FindChild("text").GetComponent<GUIText>().text=text;
+			//m_transform.Translate(new Vector3(0,0,0.1f));
+			
+			charactorPosition=Camera.main.WorldToViewportPoint(GameObject.FindGameObjectWithTag("Player").transform.position);
+			
+			m_transform.position=new Vector3(charactorPosition.x,charactorPosition.y+up,charactorPosition.z);
+			
+			up+=0.007f;
 		}
-		if(timer<=0)
-			Destroy(this.gameObject);
+		
+		
+			
 	}
 }
